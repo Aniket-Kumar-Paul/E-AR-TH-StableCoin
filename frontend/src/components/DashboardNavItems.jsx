3dec1c59c679a21009a5882991e7cc63bc998ca9
Aniket <aniketkumarpaul1@gmail.com>
2022-10-05 21:37:02 +0530

    change colour of DashboardNavItems to blue

--- frontend/src/components/DashboardNavItems.jsx
+++ frontend/src/components/DashboardNavItems.jsx
@@ -5,7 +5,7 @@ const DashboardNavItems = ({ active, link, text, color }) => {
     // const hoverColor = color.slice(0, color.length - 3) + (parseInt(color.slice(-3)) - 100).toString()
 
     return (
-        <Link to={link} className={`${color} ${active && "border-b-4 border-black"} hover:bg-blue-500 w-[250px] text-white shadow-2xl rounded-lg flex justify-center items-center p-5`}>
+        <Link to={link} className={`bg-blue-500 border-4 border-blue-700 ${active && "border-b-4 border-black"} hover:bg-blue-600 w-[250px] text-white shadow-2xl rounded-lg flex justify-center items-center p-5`}>
             <span className="font-bold text-lg text-center">
                 {text}
             </span>
